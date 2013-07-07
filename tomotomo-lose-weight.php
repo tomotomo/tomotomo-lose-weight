c85dfbe1bcf2d637babc84fae698538b6de9c282
tomotomo <valueshare@gmail.com>
2013-07-07 21:27:07 +0900

    Show burn-down-chart on your dashboard

--- tomotomo-lose-weight.php
+++ tomotomo-lose-weight.php
@@ -20,3 +20,14 @@ function current_weight_field () {
 <?php
 	
 } // End of current_weight_field
+
+
+// ダッシュボードに体重のバーンダウンチャートを表示したい
+add_action('wp_dashboard_setup', 'burndown_chart_of_own_weight' );
+function burndown_chart_of_own_weight () {
+	wp_add_dashboard_widget('tomotomo-lose-weight', '体重のバーンダウンチャート', 'burndown_chart_of_own_weight_content');
+}
+
+function burndown_chart_of_own_weight_content () {
+	echo 'ここにバーンダウンチャートが表示されちゃう？';
+}
